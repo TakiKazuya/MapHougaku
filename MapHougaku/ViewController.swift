e6ed3f2d4159f704747a6f594728df40cf4372ab
kazup1125 <kazup1125@me.com>
2020-01-23 14:08:59 +0900

    基本的なマップ機能　実装 ・自分の位置を表示 ・詳細な地図

--- MapHougaku/ViewController.swift
+++ MapHougaku/ViewController.swift
@@ -7,14 +7,67 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController,CLLocationManagerDelegate {
+    
+    //マップキット
+    @IBOutlet weak var mapView: MKMapView!
+    
+    //LocationManagerの生成
+    var locManager: CLLocationManager!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        //ここからが現在地取得の処理
+        locManager = CLLocationManager()
+        locManager.delegate = self
+        
+        initMap()
+        
+        // 位置情報の使用の許可を得る
+        locManager.requestWhenInUseAuthorization()
+        if CLLocationManager.locationServicesEnabled() {
+            switch CLLocationManager.authorizationStatus() {
+            case .authorizedWhenInUse:
+                // 座標の表示
+                locManager.startUpdatingLocation()
+                break
+            default:
+                break
+            }
+        }
+        
+        mapView.mapType = .mutedStandard
+        
     }
-
-
+    
+    func initMap() {
+        // 縮尺を設定
+        var region:MKCoordinateRegion = mapView.region
+        region.span.latitudeDelta = 0.02
+        region.span.longitudeDelta = 0.02
+        mapView.setRegion(region,animated:true)
+        
+        // 現在位置表示の有効化
+        mapView.showsUserLocation = true
+        // 現在位置設定（デバイスの動きとしてこの時の一回だけ中心位置が現在位置で更新される）
+        mapView.userTrackingMode = .follow
+    }
+    
+    func updateCurrentPos(_ coordinate:CLLocationCoordinate2D) {
+        var region:MKCoordinateRegion = mapView.region
+        region.center = coordinate
+        mapView.setRegion(region,animated:true)
+    }
+    
+    // CLLocationManagerのdelegate：現在位置取得
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations:[CLLocation]) {
+        mapView.userTrackingMode = .follow
+    }
+    
+    
 }
 
